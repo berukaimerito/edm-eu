2ed62b32a98336857cf0bf66ee90c039031870fe
berukaimerito <kocakberkaymert@gmail.com>
2024-09-30 23:44:36 +0200

    changes2

--- app/components/Carousel/CarouselHome.tsx
+++ app/components/Carousel/CarouselHome.tsx
@@ -3,7 +3,6 @@
 
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import Image from 'next/image';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { carouselImages, CarouselImages } from './carouselImages';
 
@@ -15,23 +14,25 @@ const CarouselHome: React.FC = () => {
         infiniteLoop
         showThumbs={false}
         showStatus={false}
-        showIndicators={false} // Hide indicators if not needed
+        showIndicators={false}
         interval={8000}
         dynamicHeight={false}
         emulateTouch={true}
         swipeable
-        stopOnHover={false} // Prevent pausing on hover
+        stopOnHover={false}
         className="carousel-container"
       >
         {carouselImages.map((image: CarouselImages, index: number) => (
-          <div key={index} className="relative">
-            <Image
+          <div
+            key={index}
+            className="flex justify-center items-center bg-white"
+            style={{ height: 'auto' }}
+          >
+            {/* Use the standard <img> tag to display images without cropping */}
+            <img
               src={image.src}
               alt={image.alt}
-              width={1920}
-              height={1080}
-              className="object-cover w-full h-64 sm:h-80 md:h-96 lg:h-[32rem]"
-              priority={index === 0} // Prioritize loading the first image
+              className="w-full h-auto object-contain"
             />
           </div>
         ))}
